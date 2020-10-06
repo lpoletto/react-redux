5d18f073f7bd6c63fb70235aab542f611f7c61bc
Lautaro <lautaropoletto@gmail.com>
2020-10-06 20:55:10 -0300

    docs creado

---
+++ docs/precache-manifest.5511b2c5d66e68ef0e6463aacc361935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0256a95b6bff14dd9cf9d9444c43ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad48a75b78b0b000e94b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8401fa638228db8ae0b1",
+    "url": "/static/js/2.e3298c31.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.e3298c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad48a75b78b0b000e94b",
+    "url": "/static/js/main.6e8a94af.chunk.js"
+  },
+  {
+    "revision": "3d4df75eead0d53dc3f2",
+    "url": "/static/js/runtime-main.c27813f8.js"
+  }
+]);
